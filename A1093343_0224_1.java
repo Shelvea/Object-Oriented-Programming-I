9594fb4fa9e73e0c142df95d7fa95cd3358d1675
Shelvea 菲雅 <125584161+Shelvea@users.noreply.github.com>
2023-02-27 17:50:22 +0800

    Delete A1093343_0224_1.java

--- A1093343_0224_1.java
+++
@@ -1,7 +0,0 @@
-public class A1093343_0224_1 {
-    public static void main(String[] args) {
-        int result;
-        result = 20 >> 1 + 1 << 1 * 3;
-        System.out.println("20 >> 1 + 1 << 1 * 3 = " + result);
-    }
-}
